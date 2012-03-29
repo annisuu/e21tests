16b2b69f80997ef2acea7e63b3c04912ecb345c3
annmf89 <annmf89@72a51c71-3d62-73c6-c08a-164d96d56ba9>
2012-03-29 17:40:22 +0000

    Modificacion en vistas

--- trunk/grails-app/domain/DoQuestion.groovy
+++ trunk/grails-app/domain/DoQuestion.groovy
@@ -10,5 +10,41 @@ class DoQuestion {
   String answer3
   String image3
   String tf3
+  static constraints={
+    id_test(blank:false,nullable:false)
+    questiontext(blank:false,nullable:false)
+    answer1(blank:false,nullable:false)
+    image1(blank:false,nullable:false)
+    tf1(blank:false,nullable:false)
+    answer2(blank:false,nullable:false)
+    image2(blank:false,nullable:false)
+    tf2(blank:false,nullable:false)
+    answer3(blank:false,nullable:false)
+    image3(blank:false,nullable:false)
+    tf3(blank:false,nullable:false)
+
+
+
+  }
+  static mapping={
+     table 'do_question'
+    version false
+    columns{
+      id column: 'id_question'
+      id_test column: 'id_test'
+      questiontext column: 'questiontext'
+      answer1 column: 'answer1'
+      image1 column: 'image1'
+      tf1 column: 'tf1'
+      answer2 column: 'answer2'
+      image2 column: 'image2'
+      tf2 column: 'tf2'
+      answer3 column: 'answer3'
+      image3 column: 'image3'
+      tf3 column: 'tf3'
+
+    }
+
+  }
 
 }
