30739c67011438d9ab2b6bf54c236848022331a4
annmf89 <annmf89@72a51c71-3d62-73c6-c08a-164d96d56ba9>
2012-04-02 22:40:19 +0000

    Sesiones 2

---
+++ trunk/grails-app/controllers/LoginController.groovy
@@ -0,0 +1,42 @@
+class LoginController {
+    def ConsultaService
+    def index = {redirect(action:login,params:params) }
+    def login = {}
+    def manejaLogin={
+      def user=ConsultaService.findUserforLogin(params.pasword)
+      if(!user)
+      {
+        flash.message="Usuario no encontrado"
+        redirect(action:'login')
+        return
+      }
+      else
+      {
+        session.user=user
+        println user.id_rol
+        if(user.id_rol==1)
+        {
+          println "Is Admin"
+        redirect(controller:'area')
+        }
+        if(user.id_rol==2)
+        {
+           println "Is Instructor"
+        redirect(controller:'doQuestion')
+        }
+        if(user.id_rol==3)
+        {
+           println "Is User"
+        redirect(controller:'test')
+        }
+      }
+    }
+  def logout={
+     if(session.user){
+       session.user=null
+       redirect(action:'login')
+
+  }
+  }
+
+}
