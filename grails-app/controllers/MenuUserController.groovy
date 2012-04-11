46e1a700ef9a3c7c7a47c7fbf1d286778806a2e2
MisaHitch <MisaHitch@72a51c71-3d62-73c6-c08a-164d96d56ba9>
2012-04-11 04:22:11 +0000

    usuario

--- grails-app/controllers/MenuUserController.groovy
+++ grails-app/controllers/MenuUserController.groovy
@@ -4,12 +4,14 @@ class MenuUserController {
     println "menu User --->"+params.idu
   def user=ConsultaService.findUserforLogin(params.idu)
       println user.id_area
-     redirect(action:menuUser,id:user.id_area)
-   }
+     def tematicas=ConsultaService.findTematicas(user.id_area);
+ 
+   render(view:menuUser,tematicas:tematicas)
+    }
    
   def menuUser={
    // def tematicas=ConsultaService.findTematicas(user.id_area);
-    println "tematicas-->"+id
+   // println "tematicas-->"+id
   //  render(view:menuUser,tematicas:tematicas)
 
   }
